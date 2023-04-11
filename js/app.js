610ea5a7d12dfac2ae295aabd07e78248c531164
Malek Al-Sheekh Hasan <malek.alsheekh01@gmail.com>
2023-04-11 18:48:26 +0000

    edit js file and insert a new features

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 "use strict";
 let empArr = [];
+let empForm = document.getElementById("empForm");
+let container = document.getElementById("container");
 function Employee(empId, fullName, department, level) {
   this.empId = empId;
   this.fullName = fullName;
@@ -28,19 +30,47 @@ Employee.prototype.CalcNetSalary = function () {
   return netSalary;
 };
 Employee.prototype.render = function () {
-  return document.write(
-    `<h2>Employee Name: ${
-      this.fullName
-    }</h2> and his/her <h2>Salary ${this.calcSalary()} </h2> <hr/>`
-  );
+  let empFullName = document.createElement("h2");
+  let empSalary = document.createElement("h2");
+  empFullName.textContent = `Employee Name: ${this.fullName}`;
+  empSalary.textContent = `Salary: ${this.calcSalary()}`;
+  container.appendChild(empFullName);
+  container.appendChild(empSalary); //add (line:33-38) when we took the new lecture
+  // return document.write(
+  //   `<h2>Employee Name: ${
+  //     this.fullName
+  //   }</h2> and his/her <h2>Salary ${this.calcSalary()} </h2> <hr/>`
+  // );
 };
-let ghazi = new Employee("1000", "Ghazi Samer", "Administration", "Senior");
-let lana = new Employee("1001", "Lana Ali", "Finance", "Senior");
-let tamara = new Employee("1002", "Tamara Ayoub", "Marketing", "Senior");
-let safi = new Employee("1003", "Safi Walid", "Administration", "Mid-Senior");
-let omar = new Employee("1004", "Omar Zaid", "Development", "Senior");
-let rana = new Employee("1005", "Rana Saleh", "Development", "Junior");
-let hadi = new Employee("1006", "Hadi Ahmad", "Finance", "Mid-Senior");
-for (let i = 0; i < empArr.length; i++) {
-  empArr[i].render();
+let ghazi = new Employee(1000, "Ghazi Samer", "Administration", "Senior");
+let lana = new Employee(1001, "Lana Ali", "Finance", "Senior");
+let tamara = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior");
+let safi = new Employee(1003, "Safi Walid", "Administration", "Mid-Senior");
+let omar = new Employee(1004, "Omar Zaid", "Development", "Senior");
+let rana = new Employee(1005, "Rana Saleh", "Development", "Junior");
+let hadi = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior");
+function display() {
+  for (let i = 0; i < empArr.length; i++) {
+    empArr[i].render();
+  }
 }
+
+empForm.addEventListener("submit", handelForm);
+function handelForm(event) {
+  event.preventDefault();
+  let fullName = event.target.fullName.value;
+  let empDepartment = event.target.dept.value;
+  let empLevel = event.target.level.value;
+  let empImg = event.target.img.value;
+  let empId = createEmpID();
+  let newEmp = new Employee(empId, fullName, empDepartment, empLevel);
+  newEmp.empImage = empImg;
+  newEmp.calcSalary();
+  newEmp.CalcNetSalary();
+  newEmp.render();
+  console.log(newEmp);
+}
+function createEmpID() {
+  return Math.floor(Math.random() * (10000 - 1000)) + 1000;
+}
+display();
